a2dd7158958c3adb42ba62257b890f5f212b7783
ttomita <tmtomita87@gmail.com>
2015-03-08 23:46:06 -0400

    updated Qing rank LOL code

--- Code/Qing_rank_LOL.m
+++ Code/Qing_rank_LOL.m
@@ -16,6 +16,8 @@ p(colons(endGit-1):colons(endGit)-1)=[];
 end
 addpath(p);
 
+MarkerSize = 10;
+
 [data,txt,raw] = xlsread('Qing Dataset 70 Samples Unnormalized (12-16-2014).xlsx');
 Xtrain = data(:,1:46)';
 Xtrain_rank = passtorank(Xtrain);
@@ -31,10 +33,14 @@ parms.Kmax = 3;
 Proj = LOL(Xtrain_rank,Ystr,parms.types,parms.Kmax);
 Xtrain_LOL = Xtrain_rank*transpose(Proj{1}.V);
 Xtest_LOL = Xtest_rank*transpose(Proj{1}.V);
+Xall_LOL = cat(1,Xtrain_LOL,Xtest_LOL);
+Maxs = max(Xall_LOL);
+Mins = min(Xall_LOL);
 
 linclass = fitcdiscr(Xtrain_LOL,Ystr);
+Yhats = predict(linclass,Xtest_LOL);
 
-plot3(Xtrain_LOL(Ytrain==0,1),Xtrain_LOL(Ytrain==0,2),Xtrain_LOL(Ytrain==0,3),'bo',Xtrain_LOL(Ytrain==1,1),Xtrain_LOL(Ytrain==1,2),Xtrain_LOL(Ytrain==1,3),'rx',Xtest_LOL(:,1),Xtest_LOL(:,2),Xtest_LOL(:,3),'k.')
+plot3(Xtrain_LOL(Ytrain==0,1),Xtrain_LOL(Ytrain==0,2),Xtrain_LOL(Ytrain==0,3),'bo',Xtrain_LOL(Ytrain==1,1),Xtrain_LOL(Ytrain==1,2),Xtrain_LOL(Ytrain==1,3),'rx',Xtest_LOL(strcmp(Yhats,'0'),1),Xtest_LOL(strcmp(Yhats,'0'),2),Xtest_LOL(strcmp(Yhats,'0'),3),'ko',Xtest_LOL(strcmp(Yhats,'1'),1),Xtest_LOL(strcmp(Yhats,'1'),2),Xtest_LOL(strcmp(Yhats,'1'),3),'kx')
 set(gcf,'Visible','On')
 legend('Normal','Cancer','Unknown')
 
@@ -44,25 +50,44 @@ x1 = linspace(-11e4,-1e4);
 x2 = linspace(-800,-100);
 [x1_hyper,x2_hyper] = meshgrid(x1,x2);
 x3_hyper = (-L(1)*x1_hyper - L(2)*x2_hyper - K)/L(3);
+Maxs = max(Xall_LOL);
+Mins = min(Xall_LOL);
+ax = gca;
+ax.XLim = [Mins(1) Maxs(1)];
+ax.YLim = [Mins(2) Maxs(2)];
+ax.ZLim = [Mins(3) Maxs(3)];
 hold on
 sf = surf(x1_hyper,x2_hyper,x3_hyper);
+xmax = max(Xall_LOL(:,1));
+xmin = min(Xall_LOL(:,1));
+ymax = max(Xall_LOL(:,2));
+ymin = min(Xall_LOL(:,2));
+zmax = max(Xall_LOL(:,3));
+zmin = min(Xall_LOL(:,3));
+ax = gca;
+ax.XLim = [xmin xmax];
+ax.YLim = [ymin ymax];
+ax.ZLim = [zmin zmax];
 sf.FaceColor = 'k';
 sf.FaceAlpha = 0.4;
 sf.EdgeAlpha = 0;
 ln = findobj(gca,'Type','Line');
-ln(1).Marker = '.';
-ln(2).Marker = '.';
-ln(3).Marker = '.';
-ln(1).MarkerSize = 14;
-ln(2).MarkerSize = 14;
-ln(3).MarkerSize = 14;
+ln(1).MarkerSize = MarkerSize;
+ln(2).MarkerSize = MarkerSize;
+ln(3).MarkerSize = MarkerSize;
+ln(4).MarkerSize = MarkerSize;
+ln(1).LineWidth = 1;
+ln(2).LineWidth = 1;
+ln(3).LineWidth = 1;
+ln(4).LineWidth = 1;
 ln(1).Color = 'm';
-ln(2).Color = 'c';
-ln(3).Color = 'g';
+ln(2).Color = 'm';
+ln(3).Color = 'c';
+ln(4).Color = 'g';
 grid on
 xlabel('k1')
 ylabel('k2')
 zlabel('k3')
 title('LOL on Ranked Data')
-fname = '~/Documents/MATLAB/CancerAnalysis/Plots/Qing_rank_LOL';
-save_fig(gcf,fname)
+%fname = '~/Documents/MATLAB/CancerAnalysis/Plots/Qing_rank_LOL';
+%save_fig(gcf,fname)
