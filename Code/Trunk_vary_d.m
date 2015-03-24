a3d67bfec24e20ef2786234545277fa5c925fb0f
ttomita <tmtomita87@gmail.com>
2015-03-24 17:51:33 -0400

    added new MNIST plot

--- Code/Trunk_vary_d.m
+++ Code/Trunk_vary_d.m
@@ -18,14 +18,15 @@ n = 100;
 dims = round(logspace(log10(2),3,5));
 ntrials = 10;
 ntrees = 1000;
-cumberr = NaN(ntrials,length(dims));
+cumrferr = NaN(ntrials,length(dims));
 cumf1err = NaN(ntrials,length(dims));
 cumf2err = NaN(ntrials,length(dims));
 cumf3err = NaN(ntrials,length(dims));
-tb = NaN(ntrials,length(dims));
+trf = NaN(ntrials,length(dims));
 tf1 = NaN(ntrials,length(dims));
 tf2 = NaN(ntrials,length(dims));
 tf3 = NaN(ntrials,length(dims));
+Class = [0;1];
 
 for trial = 1:ntrials
     fprintf('trial %d\n',trial)
@@ -36,42 +37,40 @@ for trial = 1:ntrials
         nvartosample = ceil(d^(2/3));
         mu1 = 1./d_idx;
         mu0 = -1*mu1;
-        mu = cat(1,mu1,mu0);
+        mu = cat(1,mu0,mu1);
         Sigma = ones(1,d);
         obj = gmdistribution(mu,Sigma);
         [X,idx] = random(obj,n);
-        Y = idx;
-        Y(idx==2) = 0;
-        Ystr = cellstr(num2str(Y));
+        Y = cellstr(num2str(Class(idx)));
         
         tic
-        b = rpclassificationforest2(ntrees,X,Ystr,'nvartosample',nvartosample,'mdiff','off','RandomForest',true);
-        tb(trial,i) = toc;
-        berr = oobpredict(b,X,Ystr,'every');
-        cumberr(trial,i) = berr(end);
+        rf = rpclassificationforest2(ntrees,X,Y,'nvartosample',nvartosample,'mdiff','off','RandomForest',true);
+        trf(trial,i) = toc;
+        berr = oobpredict(rf,X,Y,'every');
+        cumrferr(trial,i) = berr(end);
         
         fprintf('Random Forest complete\n')
         
         tic
-        f1 = rpclassificationforest2(ntrees,X,Ystr,'s',3,'nvartosample',nvartosample,'mdiff','off','sparsemethod','old');
+        f1 = rpclassificationforest2(ntrees,X,Y,'s',3,'nvartosample',nvartosample,'mdiff','off','sparsemethod','old');
         tf1(trial,i) = toc;
-        f1err = oobpredict(f1,X,Ystr,'every');
+        f1err = oobpredict(f1,X,Y,'every');
         cumf1err(trial,i) = f1err(end);
         
         fprintf('TylerForest complete\n')
         
         tic
-        f2 = rpclassificationforest2(ntrees,X,Ystr,'s',3,'nvartosample',nvartosample,'mdiff','off','sparsemethod','new');
+        f2 = rpclassificationforest2(ntrees,X,Y,'s',3,'nvartosample',nvartosample,'mdiff','off','sparsemethod','new');
         tf2(trial,i) = toc;
-        f2err = oobpredict(f2,X,Ystr,'every');
+        f2err = oobpredict(f2,X,Y,'every');
         cumf2err(trial,i) = f2err(end);
         
         fprintf('TylerForest+ complete\n')
         
         tic
-        f3 = rpclassificationforest2(ntrees,X,Ystr,'s',3,'nvartosample',nvartosample,'mdiff','on','sparsemethod','new');
+        f3 = rpclassificationforest2(ntrees,X,Y,'s',3,'nvartosample',nvartosample,'mdiff','on','sparsemethod','new');
         tf3(trial,i) = toc;
-        f3err = oobpredict(f3,X,Ystr,'every');
+        f3err = oobpredict(f3,X,Y,'every');
         cumf3err(trial,i) = f3err(end);
         
         fprintf('TylerForest+meandiff complete\n')
@@ -79,16 +78,16 @@ for trial = 1:ntrials
 end
 
 save('Trunk_vary_d.mat','cumberr','cumf1err','cumf2err','cumf3err','tb','tf1','tf2','tf3')
-bsem = std(cumberr)/sqrt(ntrials);
+rfsem = std(cumrferr)/sqrt(ntrials);
 f1sem = std(cumf1err)/sqrt(ntrials);
 f2sem  = std(cumf2err)/sqrt(ntrials);
 f3sem = std(cumf3err)/sqrt(ntrials);
-cumberr = mean(cumberr);
+cumrferr = mean(cumrferr);
 cumf1err = mean(cumf1err);
 cumf2err = mean(cumf2err);
 cumf3err = mean(cumf3err);
-Ynames = {'cumberr' 'cumf1err' 'cumf2err' 'cumf3err'};
-Enames = {'bsem' 'f1sem' 'f2sem' 'f3sem'};
+Ynames = {'cumrferr' 'cumf1err' 'cumf2err' 'cumf3err'};
+Enames = {'rfsem' 'f1sem' 'f2sem' 'f3sem'};
 lspec = {'-bo','-rx','-gd','-ks'};
 hold on
 for i = 1:length(Ynames)
@@ -103,16 +102,16 @@ fname = sprintf('Trunk/trunk_ooberror_vs_d_n%d_var%d_embed%d_ntrees%d_ntrials%d'
 save_fig(gcf,fname)
 
 figure(2)
-bsem = std(tb)/sqrt(ntrials);
+rfsem = std(trf)/sqrt(ntrials);
 f1sem = std(tf1)/sqrt(ntrials);
 f2sem  = std(tf2)/sqrt(ntrials);
 f3sem = std(tf3)/sqrt(ntrials);
-tb = mean(tb);
+trf = mean(trf);
 tf1 = mean(tf1);
 tf2 = mean(tf2);
 tf3 = mean(tf3);
-Ynames = {'tb' 'tf1' 'tf2' 'tf3'};
-Enames = {'bsem' 'f1sem' 'f2sem' 'f3sem'};
+Ynames = {'trf' 'tf1' 'tf2' 'tf3'};
+Enames = {'rfsem' 'f1sem' 'f2sem' 'f3sem'};
 lspec = {'-bo','-rx','-gd','-ks'};
 hold on
 for i = 1:length(Ynames)
