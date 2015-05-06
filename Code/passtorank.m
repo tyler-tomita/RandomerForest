b6b6654ef0a875d29d529278ad20ebeea9f3311d
ttomita <tmtomita87@gmail.com>
2015-05-06 15:36:20 -0400

    Added comments to auxillary functions for ranking data

--- Code/passtorank.m
+++ Code/passtorank.m
@@ -1,6 +1,9 @@
-%Passes each column to ranks
-
 function R = passtorank(A)
+%Passes each column of a matrix A to ranks
+
+%A: n x d matrix
+%R: represents the matrix obtained by converting each element of A to its
+%column marginal rank
     [S,i] = sort(A,1);
     i_adjust = repmat(0:size(i,1):(size(i,2)-1)*size(i,1),size(i,1),1);
     i = i + i_adjust;
