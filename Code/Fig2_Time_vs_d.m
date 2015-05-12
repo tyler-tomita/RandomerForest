07ccc65dcd083acdf8fff327aebdbf05e6a1ecf2
ttomita <tmtomita87@gmail.com>
2015-05-12 02:01:32 -0400

    increased font size of figs

--- Code/Fig2_Time_vs_d.m
+++ Code/Fig2_Time_vs_d.m
@@ -23,18 +23,19 @@ LineWidth = 1.5;
 Marker = 'none';
 Title = {'Trunk' 'Parity' 'Multimodal'};
 Units = 'pixels';
-FigPosition = [0 140 1150 325];
-Axis_Left = [50 350 650];
-Axis_Bottom = 50;
+FigPosition = [0 140 1300 350];
+Axis_Left = [60 385 710];
+Axis_Bottom = 63;
 Axis_Width = 250;
 Axis_Height = 250;
 Legend_Width = 150;
 Legend_Height = 80;
-Legend_Left = 950;
-Legend_Bottom = round(Axis_Bottom+Axis_Height/2-Legend_Height);
+Legend_Left = 1075;
+Legend_Bottom = round(Axis_Bottom+Axis_Height/2-Legend_Height/2);
 MarkerSize = 14;
 Box = 'off';
 Box_Legend = 'off';
+FontSize = 20;
 
 BasePath = '~/LOVEFest/Figures/fig/';
 Filename = {'Trunk_time_vs_d_n100_var1_ntrees1000_ntrials10_v2.fig'...
@@ -47,7 +48,7 @@ for i = 1:length(Filename)
 end
 
 h{4} = figure('Visible','On');
-set(h{4},'Position',FigPosition,'PaperOrientation','landscape','PaperUnits','inches','PaperSize',[8.5*2 11*2],'PaperPositionMode','auto','Color',Fig_Color)
+set(h{4},'Position',FigPosition,'PaperOrientation','landscape','PaperUnits','inches','PaperSize',[8.5*2.1 11*2.1],'PaperPositionMode','auto','Color',Fig_Color)
 
 for i = 1:length(h)-1
     ax_old = get(h{i},'CurrentAxes');
@@ -66,15 +67,15 @@ for i = 1:length(h)-1
     ymax_idx = find(ymax_ln==ymax_ax);
     ymax_ax = ymax_ax + h_lines(ymax_idx).UData(find(h_lines(ymax_idx).YData==ymax_ln(ymax_idx)));
     XTick = logspace(0,log10(xmax),log10(xmax)+1);
-    set(ax_new,'XLim',[0 10^(log10(xmax)+0.1)],'YLim',[0 ymax_ax],'XScale','log','XTick',XTick,'XGrid','Off','YGrid','Off','Box',Box,'LineWidth',LineWidth,'Units',Units,'Position',[Axis_Left(i) Axis_Bottom Axis_Width Axis_Height])
+    set(ax_new,'FontSize',FontSize,'XLim',[0 10^(log10(xmax)+0.1)],'YLim',[0 ymax_ax],'XScale','log','XTick',XTick,'XGrid','Off','YGrid','Off','Box',Box,'LineWidth',LineWidth,'Units',Units,'Position',[Axis_Left(i) Axis_Bottom Axis_Width Axis_Height])
     title(Title{i})
-    xlabel('Number of Ambient Dimensions')
+    xlabel('# of Ambient Dimensions')
     ylabel('Training Time (sec)')
-    hL = legend(ax_new,'Random Forest','Dense Randomer Forest','Sparse Randomer Forest','Sparse Randomer Forest w/ Mean Diff');
+    hL = legend(ax_new,'Random Forest','Dense Rmer Forest','Sp. Rmer Forest','Sp. Rmer Forest w/ MD');
     legend(ax_new,'hide')
     get(ax_new,'Position');
 end
-set(hL,'Units',Units,'Position',[Legend_Left Legend_Bottom Legend_Width Legend_Height],'Visible','On','Box',Box_Legend)
+set(hL,'FontSize',FontSize,'Units',Units,'Position',[Legend_Left Legend_Bottom Legend_Width Legend_Height],'Visible','On','Box',Box_Legend)
 
 fname = '~/LOVEFest/Figures/Fig2_Time';
 save_fig(gcf,fname)
