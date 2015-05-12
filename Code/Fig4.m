761fc4dc14f16fb75e94628e0c8979fdc6378523
ttomita <tmtomita87@gmail.com>
2015-05-11 22:53:36 -0400

    added tex file

---
+++ Code/Fig4.m
@@ -0,0 +1,88 @@
+%Make Fig1 for LOVEFest (Randomer Forest)
+
+close all
+clear
+clc
+
+fpath = mfilename('fullpath');
+findex = strfind(fpath,'/');
+rootDir=fpath(1:findex(end-1));
+p = genpath(rootDir);
+gits=strfind(p,'.git');
+colons=strfind(p,':');
+for i=0:length(gits)-1
+endGit=find(colons>gits(end-i),1);
+p(colons(endGit-1):colons(endGit)-1)=[];
+end
+addpath(p);
+
+%Colors = linspecer(3,'sequential');
+Colors = repmat(linspecer(5,'sequential'),2,1);
+Fig_Color = [1 1 1];
+LineWidth = 1.5;
+Marker = {'none' 'none' 'none' 'none' 'none' '.' '.' '.' '.' '.'};
+Units = 'pixels';
+%FigPosition = [0 140 1150 650];
+FigPosition = [0 140 725 325];
+%left = [50 350 650];
+%bottom = [350 50];
+%Axis_Left = repmat(left,1,2);
+Axis_Left = [50 350];
+%Axis_Bottom = cat(2,repmat(bottom(1),1,3),repmat(bottom(2),1,3));
+Axis_Bottom = 50;
+Axis_Width = 250;
+Axis_Height = 250;
+MarkerSize = 14;
+Box = 'off';
+Box_Legend = 'off';
+Colorbar_Left = 650;
+Colorbar_Bottom = 50;
+Colorbar_Width = 25;
+Colorbar_Height = 250;
+
+BasePath = '~/LOVEFest/Figures/fig/';
+Filename = {'Fig4_Real_Data_Panel_A_v2.fig','Fig4_Real_Data_Panel_B_v2'};
+
+for i = 1:length(Filename)
+    h{i} = openfig(strcat(BasePath,Filename{i}),'invisible');
+    grid off
+end
+
+h{3} = figure('Visible','On');
+set(h{3},'Position',FigPosition,'PaperOrientation','landscape','PaperUnits','inches','PaperSize',[8.5*2 11*2],'PaperPositionMode','auto','Color',Fig_Color)
+
+ax_old = get(h{1},'CurrentAxes');
+ax_new = subplot(1,2,1);
+copyobj(allchild(ax_old),ax_new);
+h_lines = allchild(ax_new);
+for j = 1:length(h_lines)
+    set(h_lines(j),'Color',Colors(j,:),'linewidth',LineWidth,'Marker',Marker{j},'MarkerSize',MarkerSize,'MarkerFaceColor',Colors(j,:),'MarkerEdgeColor',Colors(j,:))
+end
+set(ax_new,'XGrid','Off','YGrid','Off','Box',Box,'LineWidth',LineWidth,'Units',Units,'Position',[Axis_Left(1) Axis_Bottom Axis_Width Axis_Height])
+xlabel('Training Time (sec)')
+ylabel('Lhat')
+%hL = legend(ax_new,'Random Forest','Dense Randomer Forest','Sparse Randomer Forest','Sparse Randomer Forest w/ Mean Diff','Robust Sparse Randomer Forest w/ Mean Diff');
+legend(ax_new,'hide')
+get(ax_new,'Position');
+
+ax_old = get(h{2},'CurrentAxes');
+ax_new = subplot(1,2,2);
+copyobj(allchild(ax_old),ax_new);
+h_lines = allchild(ax_new);
+set(h_lines(1),'Color','k','linewidth',LineWidth)
+set(ax_new,'XGrid','Off','YGrid','Off','Box',Box,'LineWidth',LineWidth,'Units',Units,'Position',[Axis_Left(2) Axis_Bottom Axis_Width Axis_Height])
+xlabel('Training Time (sec)')
+ylabel('Lhat')
+%hL = legend(ax_new,'Random Forest','Dense Randomer Forest','Sparse Randomer Forest','Sparse Randomer Forest w/ Mean Diff','Robust Sparse Randomer Forest w/ Mean Diff');
+legend(ax_new,'hide')
+get(ax_new,'Position');
+r = cat(1,zeros(1000,1),transpose(linspace(0,1,1001)));
+g = cat(1,transpose(linspace(0,1,1001)),transpose(linspace(0.999,0,1000)));
+b = flipud(r);
+colormap([r g b])
+h = colorbar('Units',Units,'Position',[Colorbar_Left Colorbar_Bottom Colorbar_Width Colorbar_Height]);
+    
+%set(hL,'Units',Units,'Position',[Legend_Left Legend_Bottom Legend_Width Legend_Height],'Visible','On','Box',Box_Legend)
+
+fname = '~/LOVEFest/Figures/Fig4';
+save_fig(gcf,fname)
