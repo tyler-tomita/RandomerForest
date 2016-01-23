32a7a16a4b1348bc3995d0cf380cf6946cc74f90
ttomita <tmtomita87@gmail.com>
2016-01-23 17:50:50 -0500

    updated draft w/ new figure 3 including Trunk

--- Code/Plots/Plot_fig3_transformations2.m
+++ Code/Plots/Plot_fig3_transformations2.m
@@ -14,9 +14,13 @@ axHeight = 1.3;
 %     FontSize*12+axWidth*2,FontSize*16+axWidth*3];
 % axBottom = [FontSize*8+axHeight,FontSize*8+axHeight,FontSize*8+axHeight,...
 %     FontSize*8+axHeight,FontSize*4,FontSize*4,FontSize*4,FontSize*4];
-axLeft = [FontSize*4,FontSize*5+axWidth,FontSize*6+axWidth*2,...
-    FontSize*7+axWidth*3 FontSize*8+axWidth*4];
-axBottom = [FontSize*4,FontSize*4,FontSize*4,FontSize*4 FontSize*4];
+axLeft = [FontSize*4,FontSize*6+axWidth,FontSize*7+axWidth*2,...
+    FontSize*8+axWidth*3,FontSize*9+axWidth*4,FontSize*4,...
+    FontSize*6+axWidth,FontSize*7+axWidth*2,FontSize*8+axWidth*3,...
+    FontSize*9+axWidth*4];
+axBottom = [FontSize*9+axHeight,FontSize*9+axHeight,FontSize*9+axHeight,...
+    FontSize*9+axHeight,FontSize*9+axHeight,FontSize*4,FontSize*4,...
+    FontSize*4,FontSize*4 FontSize*4];
 figWidth = axLeft(end) + axWidth + FontSize*4;
 figHeight = axBottom(1) + axHeight + FontSize*4;
 
@@ -53,7 +57,7 @@ for j = 1:length(Transformations)
 
     classifiers = fieldnames(Lhat);
     
-    ax = subplot(1,5,j);
+    ax = subplot(2,5,j);
     
     for i = 1:length(classifiers)
         cl = classifiers{i};
@@ -83,59 +87,61 @@ for j = 1:length(Transformations)
 end
 
 
-% clear Lhat sem minIdx
-% 
-% if runSims
-%     run_Trunk_transformations
-% else
-%     load Trunk_transformations.mat
-% end
-% 
-% Transformations = fieldnames(mean_err_rf);
-% 
-% for j = 1:length(Transformations)
-%     Transform = Transformations{j};
-%     
-%     [Lhat.rf,minIdx.rf] = min(mean_err_rf.(Transform)(end,:,:),[],2);
-%     [Lhat.rerf,minIdx.rerf] = min(mean_err_rerf.(Transform)(end,:,:),[],2);
-%     [Lhat.rerfdn,minIdx.rerfdn] = min(mean_err_rerfdn.(Transform)(end,:,:),[],2);
-%     [Lhat.rf_rot,minIdx.rf_rot] = min(mean_err_rf_rot.(Transform)(end,:,:),[],2);
-% 
-%     for i = 1:length(dims)
-%         sem.rf(i) = sem_rf.(Transform)(end,minIdx.rf(i),i);
-%         sem.rerf(i) = sem_rerf.(Transform)(end,minIdx.rerf(i),i);
-%         sem.rerfdn(i) = sem_rerfdn.(Transform)(end,minIdx.rerfdn(i),i);
-%         sem.rf_rot(i) = sem_rf_rot.(Transform)(end,minIdx.rf_rot(i),i);
-%     end
-% 
-%     classifiers = fieldnames(Lhat);
-%     
-%     for i = 1:length(classifiers)
-%         cl = classifiers{i};
-%         if j == 1
-%             ax(i+4) = subplot(2,4,i+4);
-%             hold on
-%         else
-%             axes(ax(i+4));
-%         end
-%         h = errorbar(dims,Lhat.(cl)(:)',sem.(cl),'LineWidth',LineWidth);
-%     end  
-% end
-% 
-% for i = 1:length(classifiers)
-%     axes(ax(i+4));
-%     title(Titles{i+4})
-%     xlabel('d')
-%     if i == 1
-%         ylabel('Error Rate')
-%     end
-%     ax(i+4).LineWidth = LineWidth;
-%     ax(i+4).FontUnits = 'inches';
-%     ax(i+4).FontSize = FontSize;
-%     ax(i+4).Units = 'inches';
-%     ax(i+4).Position = [axLeft(i+4) axBottom(i+4) axWidth axHeight];
-%     ax(i+4).Box = 'off';
-% end
+clear Lhat sem minIdx
+
+if runSims
+    run_Trunk_transformations
+else
+    load Trunk_transformations.mat
+end
+
+Transformations = fieldnames(mean_err_rf);
+
+for j = 1:length(Transformations)
+    Transform = Transformations{j};
+    
+    [Lhat.rf,minIdx.rf] = min(mean_err_rf.(Transform)(end,:,:),[],2);
+    [Lhat.rerf,minIdx.rerf] = min(mean_err_rerf.(Transform)(end,:,:),[],2);
+    [Lhat.rerfdn,minIdx.rerfdn] = min(mean_err_rerfdn.(Transform)(end,:,:),[],2);
+    [Lhat.rf_rot,minIdx.rf_rot] = min(mean_err_rf_rot.(Transform)(end,:,:),[],2);
+
+    for i = 1:length(dims)
+        sem.rf(i) = sem_rf.(Transform)(end,minIdx.rf(i),i);
+        sem.rerf(i) = sem_rerf.(Transform)(end,minIdx.rerf(i),i);
+        sem.rerfdn(i) = sem_rerfdn.(Transform)(end,minIdx.rerfdn(i),i);
+        sem.rf_rot(i) = sem_rf_rot.(Transform)(end,minIdx.rf_rot(i),i);
+    end
+
+    classifiers = fieldnames(Lhat);
+    
+    ax = subplot(2,5,j+5);
+    
+    for i = 1:length(classifiers)
+        cl = classifiers{i};
+        h = errorbar(dims,Lhat.(cl)(:)',sem.(cl),'LineWidth',LineWidth);
+        hold on
+    end
+    
+    title(['(' char('A'+j+4) ') ' Transform])
+    xlabel('d')
+    if j+5 == 6
+        ylabel('Error Rate')
+    end
+    ax.LineWidth = LineWidth;
+    ax.FontUnits = 'inches';
+    ax.FontSize = FontSize;
+    ax.Units = 'inches';
+    ax.Position = [axLeft(j+5) axBottom(j+5) axWidth axHeight];
+    ax.Box = 'off';
+    ax.XLim = [1 600];
+    ax.YLim = [0 .15];
+    ax.XScale = 'log';
+    ax.XTick = [logspace(0,2,3) 500];
+    ax.XTickLabel = {'1';'10';'100';'500'};
+    if j+5 ~= 6
+        ax.YTick = [];
+    end
+end
 
 l = legend('RF','RerF','RerFd','RotRF');
 l.Location = 'southeast';
