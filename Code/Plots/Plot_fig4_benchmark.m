490deedc42801293c8b2573dfe9e411463822acf
ttomita <tmtomita87@gmail.com>
2016-01-25 10:15:53 -0500

    updated figs

--- Code/Plots/Plot_fig4_benchmark.m
+++ Code/Plots/Plot_fig4_benchmark.m
@@ -7,14 +7,14 @@ fpath = mfilename('fullpath');
 rerfPath = fpath(1:strfind(fpath,'RandomerForest')-1);
 
 LineWidth = 2;
-FontSize = .2;
-axWidth = 2;
-axHeight = 2;
-axLeft = [FontSize*4 FontSize*8+axWidth FontSize*4 FontSize*8+axWidth];
-axBottom = [FontSize*9+axHeight FontSize*9+axHeight FontSize*4 FontSize*4];
+FontSize = .16;
+axWidth = 1.75;
+axHeight = 1.75;
+axLeft = [FontSize*4,FontSize*7+axWidth,FontSize*10+axWidth*2,...
+    FontSize*13+axWidth*3];
+axBottom = [FontSize*4,FontSize*4,FontSize*4,FontSize*4];
 figWidth = axLeft(end) + axWidth + FontSize*4;
 figHeight = axBottom(1) + axHeight + FontSize*4;
-
 fig = figure;
 fig.Units = 'inches';
 fig.PaperUnits = 'inches';
@@ -31,7 +31,7 @@ else
     load Performance_profile_untransformed.mat
 end
 
-ax = subplot(2,2,1);
+ax = subplot(1,4,1);
 hold on
 
 xmin = min(tau);
@@ -39,13 +39,13 @@ xmax = max(tau);
 ymin = .7;
 ymax = max(rho_ps(:));
 
-for i =1:length(clnames)
+for i = 1:length(clnames)-1
      plot(tau,rho_ps(:,i),'LineWidth',LineWidth)
 end
 
 title(Titles{1})
-xlabel('r_p_s')
-ylabel('F(r_p_s)')
+xlabel('Relative Performance')
+ylabel('Emperical Distribution')
 ax.LineWidth = LineWidth;
 ax.FontUnits = 'inches';
 ax.FontSize = FontSize;
@@ -63,7 +63,7 @@ else
     load Performance_profile_rotate.mat
 end
 
-ax = subplot(2,2,2);
+ax = subplot(1,4,2);
 hold on
 
 xmin = min(tau);
@@ -71,13 +71,13 @@ xmax = max(tau);
 ymin = .7;
 ymax = max(rho_ps(:));
 
-for i =1:length(clnames)
+for i = 1:length(clnames)-1
      plot(tau,rho_ps(:,i),'LineWidth',LineWidth)
 end
 
 title(Titles{2})
-xlabel('r_p_s')
-ylabel('F(r_p_s)')
+xlabel('Relative Performance')
+% ylabel('Emperical Distribution')
 ax.LineWidth = LineWidth;
 ax.FontUnits = 'inches';
 ax.FontSize = FontSize;
@@ -94,7 +94,7 @@ else
     load Performance_profile_scale.mat
 end
 
-ax = subplot(2,2,3);
+ax = subplot(1,4,3);
 hold on
 
 xmin = min(tau);
@@ -102,13 +102,13 @@ xmax = max(tau);
 ymin = .9;
 ymax = max(rho_ps(:));
 
-for i =1:length(clnames)
+for i = 1:length(clnames)-1
      plot(tau,rho_ps(:,i),'LineWidth',LineWidth)
 end
 
 title(Titles{3})
-xlabel('r_p_s')
-ylabel('F(r_p_s)')
+xlabel('Relative Performance')
+% ylabel('Emperical Distribution')
 ax.LineWidth = LineWidth;
 ax.FontUnits = 'inches';
 ax.FontSize = FontSize;
@@ -117,6 +117,7 @@ ax.Position = [axLeft(3) axBottom(3) axWidth axHeight];
 ax.Box = 'off';
 ax.XLim = [xmin xmax];
 ax.YLim = [ymin ymax];
+ax.XTick = 2:6:14;
 ax.YTick = [.9 .95 1];
 
 
@@ -126,7 +127,7 @@ else
     load Performance_profile_affine.mat
 end
 
-ax = subplot(2,2,4);
+ax = subplot(1,4,4);
 hold on
 
 xmin = min(tau);
@@ -135,13 +136,13 @@ ymin = .9;
 ymax = max(rho_ps(:));
 
 
-for i = 1:length(clnames)
+for i = 1:length(clnames)-1
      plot(tau,rho_ps(:,i),'LineWidth',LineWidth)
 end
 
 title(Titles{4})
-xlabel('r_p_s')
-ylabel('F(r_p_s)')
+xlabel('Relative Performance')
+% ylabel('Emperical Distribution')
 ax.LineWidth = LineWidth;
 ax.FontUnits = 'inches';
 ax.FontSize = FontSize;
