9086f1c766a98a9480758e07eaa66d6767fd70e1
Tyler Tomita <tmtomita87@gmail.com>
2016-05-05 13:37:33 -0400

    fix bug in Plot_benchmark_summary.m

--- Code/Plots/Plot_benchmark_summary.m
+++ Code/Plots/Plot_benchmark_summary.m
@@ -24,7 +24,7 @@ LineWidth.frc = 2;
 FontSize = .16;
 axWidth = 1.5;
 axHeight = 1.5;
-axLeft = [FontSize*5,FontSize*8+axWidth,FontSize*11+axWidth*2,FontSize*14+axWidth*3];
+axLeft = [FontSize*5,FontSize*10+axWidth,FontSize*15+axWidth*2,FontSize*20+axWidth*3];
 axBottom = [FontSize*4,FontSize*4,FontSize*4,FontSize*4];
 figWidth = axLeft(end) + axWidth + FontSize*4;
 figHeight = axBottom(1) + axHeight + FontSize*4;
@@ -69,26 +69,48 @@ for i = 1:length(contents)
     BenchmarkName = BenchmarkName{1};
     InFile = [InPath,contents(i).name];
     load(InFile)
+    fig = figure;
+    fig.Units = 'inches';
+    fig.PaperUnits = 'inches';
+    fig.Position = [0 0 figWidth figHeight];
+    fig.PaperPosition = [0 0 figWidth figHeight];
+    fig.PaperSize = [figWidth figHeight];    
     for m = 1:length(Metrics)
         Metric = Metrics{m};
         Classifiers = fieldnames(Summary.(Metric));
         ax = subplot(1,length(Metrics),m);
         hold on
         LineNames = [];
+        min_x = [];
+        min_y = [];
+        max_x = [];
+        max_y = [];
         for c = 1:length(Classifiers)
             cl = Classifiers{c};
             if length(Summary.NMIX.(cl)) <= 0
                 plot(Summary.MTRY.(cl),Summary.(Metric).(cl),...
                     'Color',Colors.(cl),'LineWidth',LineWidth.(cl))
+                min_x = [min_x min(Summary.MTRY.(cl))];
+                max_x = [max_x max(Summary.MTRY.(cl))];
+                min_y = [min_y min(Summary.(Metric).(cl))];
+                max_y = [max_y max(Summary.(Metric).(cl))];
             else
                 plot(Summary.MTRY.(cl),Summary.(Metric).(cl)(:,1),...
                     'Color',Colors.(cl),...
 		    'LineWidth',LineWidth.(cl))
+                min_x = [min_x min(Summary.MTRY.(cl)(:,1))];
+                max_x = [max_x max(Summary.MTRY.(cl)(:,1))];
+                min_y = [min_y min(Summary.(Metric).(cl)(:,1))];
+                max_y = [max_y max(Summary.(Metric).(cl)(:,1))];
             end
             LineNames = [LineNames,{ClassifierNames(cl)}];
+            min_x = min(min_x);
+            max_x = max(max_x);
+            min_y = min(min_y);
+            max_y = max(max_y);
         end
         if m == 1
-            title(sprintf('%s (n = %d, p = %d)',BenchmarkName,n(i),d(i)))
+            title(sprintf('%s (n = %d, p = %d)',strrep(BenchmarkName,'_','\_'),n(i),d(i)))
         end
         xlabel('mtry')
         ylabel(MetricNames(m))
@@ -99,6 +121,12 @@ for i = 1:length(contents)
         ax.Position = [axLeft(m),axBottom(m),axWidth,axHeight];
         ax.Box = 'off';
         ax.XScale = 'log';
+	ax.XLim = [min_x,max_x];
+        if min_y==0 && max_y==0
+	    ax.YLim = [0 1];
+        else
+	    ax.YLim = [min_y,max_y];
+        end
     end
     save_fig(gcf,[OutPath,BenchmarkName,'_summary'])
     close
