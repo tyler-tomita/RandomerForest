c6f1134c2e283d1267aaacf96a6d1f5c9fe7a685
ttomita <tmtomita87@gmail.com>
2016-06-29 11:27:59 -0400

    change structured RerF so that multiple tiles are sampled at each split node

--- Code/Experiments/run_mnist_378.m
+++ Code/Experiments/run_mnist_378.m
@@ -7,7 +7,7 @@ rerfPath = fpath(1:strfind(fpath,'RandomerForest')-1);
 
 rng(1);
 
-ns = [100 300 1000 3000 10000];
+ns = [100 300 1000];
 X = loadMNISTImages('train-images-idx3-ubyte');
 X = sparse(X');
 Y = loadMNISTLabels('train-labels-idx1-ubyte');
@@ -34,37 +34,21 @@ for k = 1:length(ns)
     end
 
     %% Structured RerF %%
-    Fs = [2 4 8 12 20];
-    Lhat.srerf{k} = NaN(length(Fs),7);
+    Lhat.srerf{k} = NaN(1,7);
 
     fprintf('Structured RerF\n')
-    
-    for i = 1:length(Fs)
-        F = Fs(i);
 
-        fprintf('F = %d\n',F)
+    ds = [ceil(p.^[0 1/4 1/2 3/4 1]) 10*p 20*p];
 
-        if F^2 <= 4
-            ds = [1:F^2 ceil((F^2).^[1.5 2])];
-        elseif F^2 > 4 && F^2 <= 100
-            ds = ceil((F^2).^[0 1/4 1/2 3/4 1 1.5 2]);
-        else
-            ds = [ceil((F^2).^[0 1/4 1/2 3/4 1]) 5*F^2 15*F^2];
-        end
+    for j = 1:length(ds)
+        d = ds(j);
+        fprintf('d = %d\n',d)
 
-        for j = 1:length(ds)
-            d = ds(j);
-            fprintf('d = %d\n',d)
-
-            dprime = ceil(d^(1/interp1(ps,slope,p)));
-
-            srerf = rpclassificationforest(ntrees,X(TrainIdx,:),Ystr(TrainIdx),...
-                'Image',true,'ih',ih,'iw',iw,'F',F,'sparsemethod',...
-                'sparse-adjusted','nvartosample',d,'dprime',dprime,...
-                'NWorkers',NWorkers,'Stratified',Stratified);
-            Predictions = oobpredict(srerf,X(TrainIdx,:),Ystr(TrainIdx));
-            Lhat.srerf{k}(i,j) = oob_error(Predictions,Ystr(TrainIdx),'last');
-        end
+        srerf = rpclassificationforest(ntrees,X(TrainIdx,:),Ystr(TrainIdx),...
+            'Image',true,'ih',ih,'iw',iw,'F',F,'sparsemethod',...
+            'sparse','nvartosample',d,'NWorkers',NWorkers,'Stratified',Stratified);
+        Predictions = oobpredict(srerf,X(TrainIdx,:),Ystr(TrainIdx));
+        Lhat.srerf{k}(j) = oob_error(Predictions,Ystr(TrainIdx),'last');
     end
 
     %% RerF %%
@@ -72,7 +56,7 @@ for k = 1:length(ns)
 
     Lhat.rerf{k} = NaN(1,7);
 
-    ds = [ceil(p.^[0 1/4 1/2 3/4 1]) 10*p 15*p];
+    ds = [ceil(p.^[0 1/4 1/2 3/4 1]) 10*p 20*p];
 
     for j = 1:length(ds)
         d = ds(j);
@@ -107,5 +91,5 @@ for k = 1:length(ns)
     end
 end
 
-save([rerfPath 'RandomerForest/Results/mnist_378_vary_n.mat'],'ns',...
-    'ntrees','Fs','Lhat')
+% save([rerfPath 'RandomerForest/Results/mnist_378_vary_n.mat'],'ns',...
+%     'ntrees','Fs','Lhat')
