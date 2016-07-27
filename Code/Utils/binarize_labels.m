7a96ef8708392ebf01ac33ec539bdb4977716205
ttomita <tmtomita87@gmail.com>
2016-07-27 16:05:59 -0400

    code for seeing how many trees are necessary to stabilize oob error on image shapes dataset for various values of d

---
+++ Code/Utils/binarize_labels.m
@@ -0,0 +1,14 @@
+function Yb = binarize_labels(Y,Labels)
+% BINARIZE(Y) encodes an N x 1 array of multi-class labels Y into an N x
+% n_classes binary matrix Yb. Labels is the set of unique class labels. The
+% columns of Yb reflect the order of Labels.
+
+n_classes = length(Labels);
+
+Yb = zeros(length(Y),n_classes);
+
+for i = 1:n_classes
+    Yb(Y==Labels(i),i) = 1;
+end
+
+end
