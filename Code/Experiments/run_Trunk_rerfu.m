5c4c2c0ae2f8ac2081887665a0df77f739ec032e
ttomita <tmtomita87@gmail.com>
2016-08-11 20:55:03 -0400

    code for RerF uniform density of nzs per column and uniform [-1,1] density for nonzero values on Trunk and Sparse parity

--- Code/Experiments/run_Trunk_rerfu.m
+++ Code/Experiments/run_Trunk_rerfu.m
@@ -8,53 +8,81 @@ rerfPath = fpath(1:strfind(fpath,'RandomerForest')-1);
 rng(1);
 
 load Trunk_data
+load Random_matrix_adjustment_factor
 
-ntrees = 1000;
-ntrials = 25;
-NWorkers = 24;
-Class = [0;1];
-
-Lhat.rerfu = NaN(length(dims),7,ntrials);
-trainTime.rerfu = NaN(length(dims),7,ntrials);
-
-for j = 1:length(dims)
-    
-    d = dims(j);
-    
-    if d <= 5
-        mtrys = [1:d ceil(d.^[1.5 2])];
-    elseif d > 5 && d <= 100
-        mtrys = ceil(d.^[0 1/4 1/2 3/4 1 1.5 2]);
+for i = 1:length(dims)
+    p = dims(i);
+    fprintf('p = %d\n',p)
+      
+    if p <= 5
+        mtrys = [1:p ceil(p.^[1.5 2])];
+    elseif p > 5 && p <= 100
+        mtrys = ceil(p.^[0 1/4 1/2 3/4 1 1.5 2]);
     else
-        mtrys = [ceil(d.^[0 1/4 1/2 3/4 1]) 5*d 10*d];
+        mtrys = [ceil(p.^[0 1/4 1/2 3/4 1]) 5*p 10*p];
     end
+
+    Params{i}.rerfu.nTrees = 1000;
+    Params{i}.rerfu.Stratified = true;
+    Params{i}.rerfu.NWorkers = 16;
+    Params{i}.rerfu.d = mtrys;
+    Params{i}.rerfu.Method = 'uniform-nnzs';
+    Params{i}.rerfu.nmix = 1:5;
+
+    OOBError{i}.rerfu = NaN(ntrials,length(Params{i}.rerfu.d));
+    OOBAUC{i}.rerfu = NaN(ntrials,length(Params{i}.rerfu.d));
+    TrainTime{i}.rerfu = NaN(ntrials,length(Params{i}.rerfu.d));
 
     for trial = 1:ntrials
 
-        fprintf('trial %d\n',trial)
-
-        i = 1;
-
-        for i = 1:length(mtrys)
-            
-            mtry = mtrys(i);
-
-            fprintf('mtry = %d\n',mtry)
-
-            poolobj = gcp('nocreate');
-            if isempty(poolobj)
-                parpool('local',NWorkers);
-            end
-
-            tic;
-            cl.rerfu = rpclassificationforest(ntrees,X{j}(:,:,trial),Y{j}(:,trial),'sparsemethod','sparse','nvartosample',mtry,'NWorkers',NWorkers,'Stratified',true);
-            trainTime.rerfu(j,i,trial) = toc;
-            Predictions = oobpredict(cl.rerfu,X{j}(:,:,trial),Y{j}(:,trial));
-            Lhat.rerfu(j,i,trial) = oob_error(Predictions,Y{j}(:,trial),'last');
-
-            clear cl
+        % train classifier
+        poolobj = gcp('nocreate');
+        if isempty(poolobj)
+            parpool('local',Params{i}.rerfu.NWorkers,...
+                'IdleTimeout',360);
         end
+
+        tic;
+        [Forest,~,TrainTime{i}.rerfu(trial,:)] = ...
+            RerF_train(Xtrain{i}(:,:,trial),Ytrain{i}(:,trial),...
+            Params{i}.rerfu);
+
+        % select best hyperparameter
+
+        for j = 1:length(Params{i}.rerfu.d)
+            Scores = rerf_oob_classprob(Forest{j},Xtrain{i}(:,:,trial),'last');
+            Predictions = predict_class(Scores,Forest{j}.classname);
+            OOBError{i}.rerfu(trial,j) = ...
+                misclassification_rate(Predictions,Ytrain{i}(:,trial),...
+                false);
+            if size(Scores,2) > 2
+                Yb = binarize_labels(Ytrain{i}(:,trial),Forest{j}.classname);
+                [~,~,~,OOBAUC{i}.rerfu(trial,j)] = ...
+                    perfcurve(Yb(:),Scores(:),'1');
+            else
+                [~,~,~,OOBAUC{i}.rerfu(trial,j)] = ...
+                    perfcurve(Ytrain{i}(:,trial),Scores(:,2),'1');
+            end
+        end
+        BestIdx = hp_optimize(OOBError{i}.rerfu(trial,:),...
+            OOBAUC{i}.rerfu(trial,:));
+        if length(BestIdx)>1
+            BestIdx = BestIdx(end);
+        end
+
+        if ~Forest{BestIdx}.Robust
+            Scores = rerf_classprob(Forest{BestIdx},Xtest{i},'last');
+        else
+            Scores = rerf_classprob(Forest{BestIdx},Xtest{i},...
+                'last',Xtrain{i}(:,:,trial));
+        end
+        Predictions = predict_class(Scores,Forest{BestIdx}.classname);
+        TestError{i}.rerfu(trial) = misclassification_rate(Predictions,...
+            Ytest{i},false);
+
+        clear Forest
+
+        save([rerfPath 'RandomerForest/Results/Trunk_rerfu.mat'],'dims',...
+            'OOBError','OOBAUC','TestError','TrainTime')
     end
 end
-
-save([rerfPath 'RandomerForest/Results/Trunk_rerfu.mat'],'dims','Lhat','trainTime')
