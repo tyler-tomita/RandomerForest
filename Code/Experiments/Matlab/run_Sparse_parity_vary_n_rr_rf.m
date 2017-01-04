0c1de3e30282ee27c6417cbe1d8527522d096b99
Tyler Tomita <tyler@Tylers-MacBook-Pro.local>
2017-01-04 04:59:34 -0500

    removed broadcast variables from rpclassificationforest.m and changed sparse parity code to load one dataset at a time rather than all at once

--- Code/Experiments/Matlab/run_Sparse_parity_vary_n_rr_rf.m
+++ Code/Experiments/Matlab/run_Sparse_parity_vary_n_rr_rf.m
@@ -7,7 +7,10 @@ rerfPath = fpath(1:strfind(fpath,'RandomerForest')-1);
 
 rng(1);
 
-load Sparse_parity_vary_n_data
+ps = [3,10,20];
+ns = {[10,100,1000], [1000,5000,50000], [1000,10000,100000]};
+ntrials = 10;
+ntest = 10e3;
 
 Classifiers = {'rr_rf'};
 
@@ -24,9 +27,13 @@ MR = cell(length(ns{1}),length(ps));
 BestIdx = cell(length(ns{1}),length(ps));
 Labels = {'0';'1'};
 
-for j = 2:length(ps)
+for j = 1:length(ps)
     p = ps(j);
     fprintf('p = %d\n',p)
+    
+    Xtest = dlmread(sprintf('/scratch/groups/jvogels3/tyler/R/Data/Sparse_parity/dat/Test/Sparse_parity_test_set_p%d.dat',p));
+    Ytest = cellstr(num2str(Xtest(:,end)));
+    Xtest(:,end) = [];
     
     if p <= 5
         mtrys = [1:p p^2 p^3];
@@ -34,7 +41,7 @@ for j = 2:length(ps)
         mtrys = ceil(p.^[1/4 1/2 3/4 1 2 3]); 
     end
     mtrys_rf = mtrys(mtrys<=p);
-      
+    
     if j ==2
         startIdx = 3;
     else
@@ -80,6 +87,10 @@ for j = 2:length(ps)
 
             for trial = 1:ntrials
                 fprintf('Trial %d\n',trial)
+                
+                Xtrain = dlmread(sprintf('/scratch/groups/jvogels3/tyler/R/Data/Sparse_parity/dat/Train/Sparse_parity_train_set_n%d_p%d_trial%d.dat',ns{j}(i),p,trial));
+                Ytrain = cellstr(num2str(Xtrain(:,end)));
+                Xtrain(:,end) = [];
 
                 % train classifier
                 poolobj = gcp('nocreate');
@@ -89,26 +100,25 @@ for j = 2:length(ps)
                 end
 
                 [Forest,~,TrainTime{i,j}.(Classifiers{c})(trial,:)] = ...
-                    RerF_train(Xtrain{i,j}(:,:,trial),...
-                    Ytrain{i,j}(:,trial),Params{i,j}.(Classifiers{c}));
+                    RerF_train(Xtrain,Ytrain,Params{i,j}.(Classifiers{c}));
 
                 % compute oob auc, oob error, and tree stats
 
                 for k = 1:length(Forest)
                     Scores = rerf_oob_classprob(Forest{k},...
-                        Xtrain{i,j}(:,:,trial),'every');
+                        Xtrain,'every');
                     for t = 1:Forest{k}.nTrees
                         Predictions = predict_class(Scores(:,:,t),Forest{k}.classname);
                         OOBError{i,j}.(Classifiers{c})(trial,k,t) = ...
-                            misclassification_rate(Predictions,Ytrain{i,j}(:,trial),...
+                            misclassification_rate(Predictions,Ytrain,...
                         false);
                         if size(Scores,2) > 2
-                            Yb = binarize_labels(Ytrain{i,j}(:,trial),Forest{k}.classname);
+                            Yb = binarize_labels(Ytrain,Forest{k}.classname);
                             [~,~,~,OOBAUC{i,j}.(Classifiers{c})(trial,k,t)] = ... 
                                 perfcurve(Yb(:),Scores((t-1)*ns{j}(i)*Params{i,j}.(Classifiers{c}).d+(1:ns{j}(i)*Params{i,j}.(Classifiers{c}).d)),'1');
                         else
                             [~,~,~,OOBAUC{i,j}.(Classifiers{c})(trial,k,t)] = ...
-                                perfcurve(Ytrain{i,j}(:,trial),Scores(:,2,t),'1');
+                                perfcurve(Ytrain,Scores(:,2,t),'1');
                         end
                     end
                     Depth{i,j}.(Classifiers{c})(trial,:,k) = forest_depth(Forest{k})';
@@ -121,7 +131,7 @@ for j = 2:length(ps)
                     end
                     NumNodes{i,j}.(Classifiers{c})(trial,:,k) = NN;
                     NumSplitNodes{i,j}.(Classifiers{c})(trial,:,k) = NS;
-                    Scores = rerf_classprob(Forest{k},Xtest{j},'last');
+                    Scores = rerf_classprob(Forest{k},Xtest,'last');
                     TestPredictions(:,trial,k) = predict_class(Scores,Forest{k}.classname);
                 end
                 
@@ -131,7 +141,7 @@ for j = 2:length(ps)
                 BestIdx{i,j}.(Classifiers{c})(trial) = BI(end);
                 
                 TestError{i,j}.(Classifiers{c})(trial) = ...
-                    misclassification_rate(TestPredictions(:,trial,BestIdx{i,j}.(Classifiers{c})(trial)),Ytest{j},false);
+                    misclassification_rate(TestPredictions(:,trial,BestIdx{i,j}.(Classifiers{c})(trial)),Ytest,false);
 
                 clear Forest
             end
