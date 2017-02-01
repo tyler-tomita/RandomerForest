3150bd1b486cf55bd8772744f5fdaa56ca1968bd
Tyler Tomita <tmtomita87@gmail.com>
2017-02-01 13:04:47 -0500

    rerun aneuploidy cross validation to store prediction scores

--- Karchin_aneuploidy/Code/Plot_aneuploidy_results.m
+++ Karchin_aneuploidy/Code/Plot_aneuploidy_results.m
@@ -6,7 +6,13 @@ load('purple2green')
 LineWidth = 2;
 FontSize = 14;
 
-d = dir('*.mat');
+fpath = mfilename('fullpath');
+rerfPath = fpath(1:strfind(fpath,'RandomerForest')-1);
+
+resultsPath = [rerfPath 'RandomerForest/Karchin_aneuploidy/Results/2017.01.31/'];
+dataPath = [rerfPath 'RandomerForest/Karchin_aneuploidy/Data/extracted/'];
+d = dir([resultsPath '*.mat']);
+
 L = zeros(length(d),5);
 fpr = zeros(length(d),5);
 ntest = zeros(length(d),1);
@@ -15,34 +21,40 @@ for i = 1:length(d)
     Fold = regexp(d(i).name,'\d*','match');
     Fold = Fold{1};
     
-    load(d(i).name)
+    load([resultsPath d(i).name])
 
     L(i,1) = TestError.rf;
     L(i,2) = TestError.rerf;
-    L(i,3) = TestError.rr_rf;
+    L(i,3) = TestError.rerf_dense;
+    L(i,4) = TestError.rerfc;
+    L(i,5) = TestError.rr_rf;
+
 
     fpr(i,1) = ConfusionMatrix.rf(1,2)/sum(ConfusionMatrix.rf(:));
     fpr(i,2) = ConfusionMatrix.rerf(1,2)/sum(ConfusionMatrix.rerf(:));
-    fpr(i,3) = ConfusionMatrix.rr_rf(1,2)/sum(ConfusionMatrix.rr_rf(:));
+    fpr(i,3) = ConfusionMatrix.rerf_dense(1,2)/sum(ConfusionMatrix.rerf_dense(:));
+    fpr(i,4) = ConfusionMatrix.rerfc(1,2)/sum(ConfusionMatrix.rerfc(:));
+    fpr(i,5) = ConfusionMatrix.rr_rf(1,2)/sum(ConfusionMatrix.rr_rf(:));
     
-    load(['../2017.01.29/Fold_' Fold '_dense.mat'])
+    Scores = [];
+    Scores(:,1) = TestScores.rf(:,2);
+    Scores(:,2) = TestScores.rerf(:,2);
+    Scores(:,3) = TestScores.rerf_dense(:,2);
+    Scores(:,4) = TestScores.rerfc(:,2);
+    Scores(:,5) = TestScores.rr_rf(:,2);
     
-    L(i,4) = TestError.rerf_dense;
+    filename = fullfile(resultsPath, [Fold '.scores.dat']);
+    fid = fopen(filename, 'wt');
+    fprintf(fid, '%s\t%s\t%s\t%s\t%s\n','RF','RerF(rho=.0127)','RerF(rho=.0253)','RerF(rho=1)','RR-RF');  % header
+    fclose(fid);
+    dlmwrite(filename,Scores,'delimiter','\t','precision','%.12f','-append');
     
-    fpr(i,4) = ConfusionMatrix.rerf_dense(1,2)/sum(ConfusionMatrix.rerf_dense(:));
-    
-    load(['../2017.01.30/Fold_' Fold '_dense.mat'])
-    
-    L(i,5) = TestError.rerfc;
-    
-    fpr(i,5) = ConfusionMatrix.rerfc(1,2)/sum(ConfusionMatrix.rerfc(:));
-    
-    X = dlmread(['../../Data/extracted/' Fold '.test.dat']);
+    X = dlmread([dataPath Fold '.test.dat']);
     ntest(i) = size(X,1);
 end
 
-L = L(:,[1,2,4,5,3]);
-fpr = fpr(:,[1,2,4,5,3]);
+% L = L(:,[1,2,4,5,3]);
+% fpr = fpr(:,[1,2,4,5,3]);
 
 Weights = ntest/sum(ntest);
 Weights = repmat(Weights,1,size(L,2));
@@ -78,31 +90,3 @@ eb.Color = ColorMap(2,:);
 ax.FontSize = FontSize;
 
 save_fig(gcf,'~/RandomerForest/Karchin_aneuploidy/Plots/Error_FPR',{'fig','pdf','png'})
-
-%%%%%%%
-
-% d = dir('*.mat');
-% L = zeros(length(d),1);
-% fpr = zeros(length(d),1);
-% ntest = zeros(length(d),1);
-% 
-% for i = 1:length(d) 
-%     S1 = load(d(i).name);
-%     Fold = regexp(d(i).name,'\d*','match');
-%     Fold = Fold{1};
-%     S2 = load(['../2017.01.27/Fold_' Fold '.mat']);
-%     S = S2;
-%     S.TestError.rerf_dense = S1.TestError.rerf_dense;
-%     
-%     L(i) = TestError.rerf_dense;
-% 
-%     fpr(i) = ConfusionMatrix.rerf_dense(1,2)/sum(ConfusionMatrix.rerf_dense(:));
-%     
-%     X = dlmread(['../../Data/extracted/' Fold '.test.dat']);
-%     ntest(i) = size(X,1);
-% end
-% 
-% Weights = ntest/sum(ntest);
-% 
-% MeanTestError = sum(L.*Weights);
-% Meanfpr = sum(fpr.*Weights);
