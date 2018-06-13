aa29beaa37834f771bbaac45907eef3b27c213f6
Tyler Tomita <tmtomita87@gmail.com>
2018-06-13 16:46:27 -0400

    update CCFPredict() to take grouping array for handling categorical features

--- Code/Utils/CCFPredict.m
+++ Code/Utils/CCFPredict.m
@@ -1,5 +1,9 @@
-function yhats = CCFPredict(xtrain,ytrain,xtest,nTrees,options)
+function yhats = CCFPredict(xtrain,ytrain,xtest,nTrees,options,iFeatureNum)
 
-    [CCF,preds,~,~] = genCCF(nTrees,xtrain,ytrain,false,options,xtest,false);
+    if isempty(iFeatureNum)
+        [CCF,preds,~,~] = genCCF(nTrees,xtrain,ytrain,false,options,xtest,false);
+    else
+        [CCF,preds,~,~] = genCCF(nTrees,xtrain,ytrain,false,options,xtest,false,iFeatureNum);
+    end
     yhats = CCF.classNames(preds);
 end
